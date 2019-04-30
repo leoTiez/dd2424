1d009461ac157c18fb6355602a522256c7e9e5ee
Leo Tiez <leozeitler95@gmail.com>
2019-04-30 12:26:27 +0200

    Update: Added bash file to download datasets into the right directory

---
+++ download-data.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+unameOut="$(uname -s)"
+
+case "${unameOut}" in
+    Linux*)     sudo apt-get update && sudo apt-get install curl;;
+    CYGWIN*)    sudo apt-get update && sudo apt-get install curl;;
+    MINGW*)     sudo apt-get update && sudo apt-get install curl;;
+esac
+
+rm -rf data/
+mkdir data
+curl https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz -o data/cifar-10-batches-py.tar.gz
+tar -xvzf data/cifar-10-batches-py.tar.gz -C data/
+
+curl https://www.cs.toronto.edu/~kriz/cifar-100-python.tar.gz -o data/cifar-100-batches-py.tar.gz
+tar -xzvf data/cifar-100-batches-py.tar.gz -C data/
