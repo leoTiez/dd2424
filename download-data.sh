f06d14dc274110cde32c54c20622bafd18fae113
Leo Tiez <leozeitler95@gmail.com>
2019-04-30 16:57:15 +0200

    Update: added mnist and downloads only if it hasn't been already
    Mnist data is downloaded from official website. It is downloaded
    instead of using the tensorflow data package to have all data
    at one place and to control the manipulation of the data.
    
    Downloads data only if it hasn't been dowloaded already

--- download-data.sh
+++ download-data.sh
@@ -1,42 +1,35 @@
 #!/usr/bin/env bash
 
-OS_NAME ="$(uname -s)"
+OS_NAME="$(uname -s)"
 
-if ["$OS_NAME" == "Linux"]; then
+if [[ "$OS_NAME" == "Linux" ]]; then
     sudo apt-get update && sudo apt-get install curl
 fi
 
-DATA_DIRECTORY="DATA"
-if [! -d "$DATA_DIRECTORY"]; then
+DATA_DIRECTORY="data"
+if [[ ! -d "$DATA_DIRECTORY" ]]; then
   mkdir data
 fi
 
-CIFAR_10_DIRECTORY="cifar-10-batches-py"
-if [! -d "$CIFAR_10_DIRECTORY"]; then
+CIFAR_10_DIRECTORY="data/cifar-10-batches-py"
+if [[ ! -d "$CIFAR_10_DIRECTORY" ]]; then
     curl https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz -o data/cifar-10-batches-py.tar.gz
-    tar -xvzf data/cifar-10-batches-py.tar.gz -C data/
+    tar -xvf data/cifar-10-batches-py.tar.gz -C data/
     rm -rf data/cifar-10-batches-py.tar.gz
 fi
 
-CIFAR_100_DIRECTORY="cifar-100-batches-py"
-if [! -d "$CIFAR_100_DIRECTORY"]; then
+CIFAR_100_DIRECTORY="data/cifar-100-python"
+if [[ ! -d "$CIFAR_100_DIRECTORY" ]]; then
     curl https://www.cs.toronto.edu/~kriz/cifar-100-python.tar.gz -o data/cifar-100-batches-py.tar.gz
-    tar -xzvf data/cifar-100-batches-py.tar.gz -C data/
-    rm -rf data/cifar-10-batches-py.tar.gz
+    tar -xvf data/cifar-100-batches-py.tar.gz -C data/
+    rm -rf data/cifar-100-batches-py.tar.gz
 fi
 
-MNIST_DIRECTORY="mnist"
-if [! -d "$MNIST_DIRECTORY"]; then
-    mkdir mnist
-    curl yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz -o data/mnist-train-images.tar.gz
-    tar -xzvf data/mnist-train-images.tar.gz -C data/
-
-    curl yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz -o data/mnist-train-labels.tar.gz
-    tar -xzvf data/mnist-train-labels.tar.gz -C data/
-
-    curl yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz -o data/mnist-test-images.tar.gz
-    tar -xzvf data/mnist-test-images.tar.gz -C data/
-
-    curl yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz -o data/mnist-test-labels.tar.gz
-    tar -xzvf data/mnist-test-labels.tar.gz -C data/
+MNIST_DIRECTORY="data/mnist"
+if [[ ! -d "$MNIST_DIRECTORY" ]]; then
+    mkdir data/mnist
+    curl yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz -o data/mnist/mnist-train-images.gz
+    curl yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz -o data/mnist/mnist-train-labels.gz
+    curl yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz -o data/mnist/mnist-test-images.gz
+    curl yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz -o data/mnist/mnist-test-labels.gz
 fi
