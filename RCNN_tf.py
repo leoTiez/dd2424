ff3b868bf99438e42e5cf74d9d6dcf08b53d3840
Leo Tiez <leozeitler95@gmail.com>
2019-05-03 21:34:40 +0200

    Update: Implemented architecture proposed by the paper
    The rcl, softmax layer as well as the pooling and the standard
    convolutional layers are implemented and are working. Results
    are promising so far. However, the accuracy indicates
    that the global max pooling is not workin properly. It is still
    necessary to investigate this issue.

--- RCNN_tf.py
+++ RCNN_tf.py
@@ -1,10 +1,13 @@
+import sys
 import tensorflow as tf
 import numpy as np
 from load_data import load_mnist, preprocess_mnist_data
 
+# Constants
 # float64 is not allowed by all tf operations
 PRECISION_TF = tf.float32
 PRECISION_NP = np.float32
+PADDING_LIST = ['SAME', 'VALID']
 
 
 def rcl(
@@ -68,6 +71,55 @@ def rcl(
     return output
 
 
+def convolutional_layer(
+        input_data,
+        filter_shape,
+        num_input_channels,
+        num_filter,
+        std=.03,
+        stride=(1, 1),
+        padding='same',
+        name='conv'
+):
+    conv_filter_shape = [filter_shape[0], filter_shape[1], num_input_channels, num_filter]
+    strides = [1, stride[0], stride[1], 1]
+
+    weights = tf.Variable(
+        tf.truncated_normal(conv_filter_shape, stddev=std, dtype=PRECISION_TF),
+        trainable=True,
+        name=name + '_weights'
+    )
+
+    if padding.upper() not in PADDING_LIST:
+        raise ValueError('Padding value is not understood')
+
+    output = tf.nn.conv2d(input_data, weights, strides=strides, padding=padding.upper())
+
+    return output
+
+
+def pooling_layer(
+        input_data,
+        pool_shape,
+        stride=(1, 1),
+        padding='same'
+):
+    ksize = [1, pool_shape[0], pool_shape[1], 1]
+    strides = [1, stride[0], stride[1], 1]
+
+    if padding.upper() not in PADDING_LIST:
+        raise ValueError('Padding value is not understood')
+
+    output = tf.nn.max_pool(input_data, ksize=ksize, strides=strides, padding=padding.upper())
+    return output
+
+
+def global_max_pooling_layer(
+        input_data
+):
+    return tf.reduce_max(input_data, axis=[1, 2])
+
+
 def softmax_layer(
         input_data,
         num_input_dim,
@@ -91,14 +143,31 @@ def softmax_layer(
     return tf.nn.softmax(linear_trans), linear_trans
 
 
+def accuracy(
+        labels,
+        result
+):
+    correct_prediction = tf.equal(
+        tf.argmax(labels, axis=1),
+        tf.argmax(result, axis=1)
+    )
+
+    return tf.reduce_mean(tf.cast(correct_prediction, PRECISION_TF))
+
+
 if __name__ == '__main__':
+    # Setting the parameters
     input_shape_ = [None, 28, 28, 1]
     output_shape_ = [None, 10]
     learning_rate_ = .1
-    epochs_ = 1
+    epochs_ = 5
+    # For me, setting the *_size to more than 2000 my system ran out of memory
+    # For the large scale tests it should not be an issue anymore and the test size can be increased
     batch_size_ = 100
-    test_data_size_ = 5000
+    test_data_size_ = 2000
+    num_filter_ = 32
 
+    # Load and transform the data
     mnist_dict_ = load_mnist('train', dtype=PRECISION_NP)
     training_data_ = mnist_dict_['data']
     training_data_, training_labels_ = preprocess_mnist_data(
@@ -108,36 +177,103 @@ if __name__ == '__main__':
     )
     training_data_ = training_data_[:-test_data_size_]
     training_labels_ = training_labels_[:-test_data_size_]
+
     test_data_ = training_data_[-test_data_size_:]
     test_labels_ = training_labels_[-test_data_size_:]
 
+    # Create input and output placeholder
     input_placeholder_ = tf.placeholder(PRECISION_TF, input_shape_)
     output_placeholder_ = tf.placeholder(PRECISION_TF, output_shape_)
 
-    rcl_layer_1_ = rcl(
-        input_data=input_placeholder_,
+    # Net definition
+    # First convolutional layer
+    conv_layer_ = convolutional_layer(
+        input_placeholder_,
         num_input_channels=1,
+        filter_shape=(3, 3),
+        num_filter=num_filter_,
+        name='conv_layer_1'
+    )
+
+    pooling_shape_1_ = (3, 3)
+    striding_shape_1_ = (2, 2)
+
+    pooling_1_ = pooling_layer(
+        input_data=conv_layer_,
+        pool_shape=pooling_shape_1_,
+        stride=striding_shape_1_
+    )
+
+    rcl_layer_1_ = rcl(
+        input_data=pooling_1_,
+        num_input_channels=num_filter_,
         filter_shape=(3,3),
-        num_filter=32,
+        num_filter=num_filter_,
         name='rcl_layer_1'
     )
 
-    flattern_dim_ = input_shape_[1] * input_shape_[2] * 32
-    flattern_ = tf.reshape(rcl_layer_1_, [-1, input_shape_[1] * input_shape_[2] * 32])
+    rcl_layer_2_ = rcl(
+        input_data=rcl_layer_1_,
+        num_input_channels=num_filter_,
+        filter_shape=(3, 3),
+        num_filter=num_filter_,
+        name='rcl_layer_2'
+    )
+
+    pooling_shape_2_ = (3, 3)
+    striding_shape_2_ = (2, 2)
+
+    pooling_2_ = pooling_layer(
+        input_data=rcl_layer_2_,
+        pool_shape=pooling_shape_2_,
+        stride=striding_shape_2_
+    )
+
+    rcl_layer_3_ = rcl(
+        input_data=pooling_2_,
+        num_input_channels=num_filter_,
+        filter_shape=(3, 3),
+        num_filter=num_filter_,
+        name='rcl_layer_3'
+    )
+
+    rcl_layer_4_ = rcl(
+        input_data=rcl_layer_3_,
+        num_input_channels=num_filter_,
+        filter_shape=(3, 3),
+        num_filter=num_filter_,
+        name='rcl_layer_4'
+    )
+
+    # global_max_ = global_max_pooling_layer(rcl_layer_4_)
+
+    # flattern_ = tf.reshape(global_max_, [-1, global_max_.shape[-1].value])
+    #
+    # result_, linear_trans_ = softmax_layer(
+    #     flattern_,
+    #     global_max_.shape[-1].value,
+    #     output_shape_[1]
+    # )
+
+    flattern_ = tf.reshape(rcl_layer_4_, [-1, rcl_layer_4_.shape[1].value * rcl_layer_4_.shape[2].value * rcl_layer_4_.shape[3].value])
 
     result_, linear_trans_ = softmax_layer(
         flattern_,
-        flattern_dim_,
+        rcl_layer_4_.shape[1].value * rcl_layer_4_.shape[2].value * rcl_layer_4_.shape[3].value,
         output_shape_[1]
     )
 
+    # cross entropy and accuracy matrix
     cross_entropy_ = tf.reduce_mean(
-        tf.nn.softmax_cross_entropy_with_logits(logits=linear_trans_, labels=output_placeholder_)
+        tf.nn.softmax_cross_entropy_with_logits_v2(logits=linear_trans_, labels=output_placeholder_)
     )
 
     optimiser_ = tf.train.AdamOptimizer(learning_rate=learning_rate_).minimize(cross_entropy_)
-    correct_prediction_ = tf.equal(tf.argmax(output_placeholder_, 1), tf.argmax(output_placeholder_, 1))
-    accuracy_ = tf.reduce_mean(tf.cast(correct_prediction_, tf.float32))
+
+    accuracy_ = accuracy(
+        output_placeholder_,
+        result_
+    )
 
     init_op_ = tf.global_variables_initializer()
 
@@ -148,6 +284,8 @@ if __name__ == '__main__':
         for epoch in range(epochs_):
             avg_cost_ = 0
             for i in range(total_batch_):
+                progress = (i / float(total_batch_-1)) * 100
+                print '\r {:.1f}%'.format(progress), '\t{0}> '.format('#' * int(progress)),
                 batch_x_ = training_data_[i * batch_size_: (i + 1) * batch_size_]
                 batch_y_ = training_labels_[i * batch_size_: (i + 1) * batch_size_]
                 _, cost_ = sess_.run([optimiser_, cross_entropy_],
@@ -156,19 +294,19 @@ if __name__ == '__main__':
                                          output_placeholder_: batch_y_
                                      })
                 avg_cost_ += cost_ / total_batch_
+
             test_acc_ = sess_.run(accuracy_,
                                   feed_dict={
                                       input_placeholder_: test_data_,
                                       output_placeholder_: test_labels_
                                   })
-            print("Epoch:", (epoch + 1), "cost =", "{:.3f}".format(avg_cost_),
-                  "test accuracy: {:.3f}".format(test_acc_))
+            print "\nEpoch:", (epoch + 1), \
+                "cost =", "{:.3f}".format(avg_cost_),\
+                "test accuracy: {:.3f}".format(test_acc_)
 
-            print("\nTraining complete!")
-            print(sess_.run(accuracy_,
-                            feed_dict={
-                                input_placeholder_: test_data_,
-                                output_placeholder_: test_labels_
-                            }
-                            )
-                  )
+        print "\nTraining complete!"
+        print sess_.run(accuracy_,
+                        feed_dict={
+                            input_placeholder_: test_data_,
+                            output_placeholder_: test_labels_
+                        })
