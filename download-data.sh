533e8f7344bae9f05a375e336880bca5d5477db8
Adrian Chmielewski-Anders <adrianc-a@users.noreply.github.com>
2019-05-03 14:01:08 +0200

    Resolve quote issue

--- download-data.sh
+++ download-data.sh
@@ -20,29 +20,29 @@ fi
 
 CIFAR_100_DIRECTORY="${DATA_DIRECTORY}/cifar-100-python"
 if [[ ! -d "$CIFAR_100_DIRECTORY" ]]; then
-    curl https://www.cs.toronto.edu/~kriz/cifar-100-python.tar.gz -o $DATA_DIRECTORY/cifar-100-batches-py.tar.gz
-    tar -xvf $DATA_DIRECTORY/cifar-100-batches-py.tar.gz -C $DATA_DIRECTORY
-    rm -rf $DATA_DIRECTORY/cifar-100-batches-py.tar.gz
+    curl https://www.cs.toronto.edu/~kriz/cifar-100-python.tar.gz -o "$DATA_DIRECTORY"/cifar-100-batches-py.tar.gz
+    tar -xvf "$DATA_DIRECTORY"/cifar-100-batches-py.tar.gz -C "$DATA_DIRECTORY"
+    rm -rf "$DATA_DIRECTORY"/cifar-100-batches-py.tar.gz
 fi
 
 MNIST_DIRECTORY="${DATA_DIRECTORY}/mnist"
 if [[ ! -d "$MNIST_DIRECTORY" ]]; then
-    mkdir $MNIST_DIRECTORY
+    mkdir "$MNIST_DIRECTORY"
     curl yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz -o \
-        $MNIST_DIRECTORY/mnist-train-images.gz
+        "$MNIST_DIRECTORY"/mnist-train-images.gz
     curl yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz -o \
-        $MNIST_DIRECTORY/mnist-train-labels.gz
+        "$MNIST_DIRECTORY"/mnist-train-labels.gz
     curl yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz -o \
-        $MNIST_DIRECTORY/mnist-test-images.gz
+        "$MNIST_DIRECTORY"/mnist-test-images.gz
     curl yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz -o \
-        $MNIST_DIRECTORY/mnist-test-labels.gz
+        "$MNIST_DIRECTORY"/mnist-test-labels.gz
 fi
 
-TINY_NET_DIRECTORY="${DATA_DIRECTORY}/tinynet"
-if [[ ! -d "$TINY_NET_DIRECTORY" ]]; then
-    mkdir $TINY_NET_DIRECTORY
+TINYNET_DIRECTORY="${DATA_DIRECTORY}/tinynet"
+if [[ ! -d "$TINYNET_DIRECTORY" ]]; then
+    mkdir $TINYNET_DIRECTORY
     curl http://cs231n.stanford.edu/tiny-imagenet-200.zip -o \
-        $TINY_NET_DIRECTORY/raw.zip
-    tar -xf $TINY_NET_DIRECTORY/raw.zip -C $TINY_NET_DIRECTORY --strip-components=1
+        $TINYNET_DIRECTORY/raw.zip
+    tar -xf $TINYNET_DIRECTORY/raw.zip -C $TINYNET_DIRECTORY --strip-components=1
 fi
 export DATA_DIRECTORY
