5e10fd8e170452dda02c1cd95568e19e89e0f88e
Adrian Chmielewski-Anders <adrianc-a@users.noreply.github.com>
2019-05-14 22:42:58 +0200

    quick and dirty grid search

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import sys
 import RCNN_tf
 from load_data import data_loader
 import numpy as np
+from os import path
+import tensorflow as tf
 
 VAL_SET_PERCENT_LEN = 10
 
@@ -15,6 +17,18 @@ def main(argv):
     else:
         raise ValueError("Device type is not supported. Use either GPU or CPU")
 
+    do_grid_search = False
+    if len(argv) > 2:
+        if argv[3] == 'grid-search':
+            do_grid_search = True
+
+    train_data_length_ = None
+    test_data_length_ = None
+
+    if do_grid_search:
+        train_data_length_ = 2000
+        test_data_length_ = 500
+
     dataset_name_ = argv[2]
     if dataset_name_.upper() == "MNIST":
         # Setting the parameters
@@ -26,8 +40,6 @@ def main(argv):
         num_filter_ = 64
         buffer_size_ = 10000
         recurrent_depth_ = 3
-        train_data_length_ = None
-        test_data_length_ = None
 
         training_data_, training_labels_ = data_loader(
             "mnist",
@@ -64,8 +76,6 @@ def main(argv):
         num_filter_ = 96
         buffer_size_ = 10000
         recurrent_depth_ = 3
-        train_data_length_ = None
-        test_data_length_ = None
 
         if not use_grayscale:
             training_data_ = np.empty((0, 32, 32, 3))
@@ -116,8 +126,6 @@ def main(argv):
         num_filter_ = 96
         buffer_size_ = 10000
         recurrent_depth_ = 3
-        train_data_length_ = None
-        test_data_length_ = None
 
         training_data_, training_labels_ = data_loader(
             "cifar100",
@@ -144,28 +152,80 @@ def main(argv):
     else:
         raise Exception("dataset has to be one of 'mnist', 'cifar10' or 'cifar100'.")
 
-    rcnn = RCNN_tf.RCNN(
-        input_shape=input_shape_,
-        output_shape=output_shape_,
-        processing_unit=processing_unit_,
-        learning_rate=learning_rate_,
-        num_filter=num_filter_,
-        shuf_buf_size=buffer_size_,
-        recurrent_depth=recurrent_depth_
-    )
+    if not do_grid_search:
+        rcnn = RCNN_tf.RCNN(
+            input_shape=input_shape_,
+            output_shape=output_shape_,
+            processing_unit=processing_unit_,
+            learning_rate=learning_rate_,
+            num_filter=num_filter_,
+            shuf_buf_size=buffer_size_,
+            recurrent_depth=recurrent_depth_
+        )
 
-    rcnn.train(
-        train_data_feats=training_data_,
-        train_data_labels=training_labels_,
-        val_data_feats=val_data_,
-        val_data_labels=val_label_,
-        test_data_feats=test_data_,
-        test_data_labels=test_labels_,
-        batch_size=batch_size_,
-        epochs=epochs_,
-        create_graph=False,
-        print_vars=True
-    )
+        rcnn.train(
+            train_data_feats=training_data_,
+            train_data_labels=training_labels_,
+            val_data_feats=val_data_,
+            val_data_labels=val_label_,
+            test_data_feats=test_data_,
+            test_data_labels=test_labels_,
+            batch_size=batch_size_,
+            epochs=epochs_,
+            create_graph=False,
+            print_vars=True
+        )
+    else:
+        fname = 'accuracies-{}.txt'.format(dataset_name_, 'w+')
+        if path.isfile(fname):
+            raise Exception('{} already exists. Do not want to overwrite. Exiting')
+        accuracies = open(fname, 'w+')
+        epochs_ = 6
+        for recurrent_depth_ in [0, 3, 6]:
+            for adaptive_learning_factor in [0.01, 0.1, 1]:
+                tf.reset_default_graph()
+                # as per the paper to set the number of weights to be roughly
+                # the same
+                print 'Training with depth {} and learning factor {}'.format(
+                    recurrent_depth_, adaptive_learning_factor
+                )
+
+                if recurrent_depth_ == 0:
+                    num_filter_ = 128
+                rcnn = RCNN_tf.RCNN(
+                    input_shape=input_shape_,
+                    output_shape=output_shape_,
+                    processing_unit=processing_unit_,
+                    learning_rate=learning_rate_,
+                    num_filter=num_filter_,
+                    shuf_buf_size=buffer_size_,
+                    recurrent_depth=recurrent_depth_
+                )
+
+                accuracy = rcnn.train(
+                    train_data_feats=training_data_,
+                    train_data_labels=training_labels_,
+                    val_data_feats=val_data_,
+                    val_data_labels=val_label_,
+                    test_data_feats=test_data_,
+                    test_data_labels=test_labels_,
+                    batch_size=batch_size_,
+                    epochs=epochs_,
+                    create_graph=False,
+                    print_vars=True,
+                    adaptive_learning_factor=adaptive_learning_factor,
+                    dir_name='{}-depth_{}-learningfactor_{}'.format(dataset_name_, recurrent_depth_, adaptive_learning_factor)
+                )
+                print 'accuracy {}'.format(accuracy)
+
+                accuracies.write(
+                    'depth = {}, learningfactor = {}: {}\n'.format(
+                        recurrent_depth_, adaptive_learning_factor, accuracy
+                    )
+                )
+                # in case we're interrupted we'll save our progress somewhat
+                accuracies.flush()
+        accuracies.close()
 
 
 if __name__ == '__main__':
