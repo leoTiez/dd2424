3f0ec3520d1893892968e36a4ff7a402c1ba032a
Adrian Chmielewski-Anders <adrianc-a@users.noreply.github.com>
2019-05-18 23:32:18 +0200

    for all datasets

--- train-3-test-6.sh
+++ train-3-test-6.sh
@@ -2,5 +2,12 @@
 
 lname="train-3-test-6"
 
-python main.py gpu mnist -dl 3 -dt 6 -f 0.1 -b 1 > ${lname}.log \
-    2> ${lname}.err.log
+
+python main.py gpu mnist -dl 3 -dt 6 -f 0.01 -b 1 > ${lname}-mnist.log \
+    2> ${lname}-mnist.err.log
+
+python main.py gpu cifar10 -dl 3 -dt 6 -f 0.1 -b 1 > ${lname}-cifar10.log \
+    2> ${lname}-cifar10.err.log
+
+python main.py gpu cifar100 -dl 3 -dt 6 -f 1 -b 1 > ${lname}-cifar100.log \
+    2> ${lname}-cifar100.err.log
